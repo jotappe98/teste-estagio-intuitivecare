600dbfe81a288fa26f453776766ea3a1be5936a1
João Pedro Andrade <jopedro172@gmail.com>
2026-02-01 23:30:18 -0300

    adiciona inserts de exemplo para carga da tabela despesas_operadoras

---
+++ teste-3-banco-de-dados/sql/02_import_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO despesas_operadoras(
+razao_social,
+uf,
+total_despesas, 
+media_trimestral, 
+desvio_padrao) VALUES
+
+('OPERADORA A', 'SP', 150000000.00, 50000000.00, 12000000.00),
+('OPERADORA B', 'RJ', 98000000.00, 32666666.67, 8000000.00);

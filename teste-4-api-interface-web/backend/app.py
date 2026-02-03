7aae41928dae19b9a5d50942877af8fe246de176
João Pedro Andrade <jopedro172@gmail.com>
2026-02-03 11:33:44 -0300

    criacao de rota /api/operadoras com paginação

--- teste-4-api-interface-web/backend/app.py
+++ teste-4-api-interface-web/backend/app.py
@@ -1,18 +1,33 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import pandas as pd
 
 app = Flask(__name__)
 
-# Carrega dados
+# Lê e carrega os dados
 df = pd.read_csv(
-    "data/despesas_agregadas.csv",
+    "backend/data/despesas_agregadas.csv",
     sep=";",
     encoding="utf-8"
 )
 
+# Rota para listar as operadoras
 @app.route("/api/operadoras", methods=["GET"])
 def listar_operadoras():
-    return jsonify(df.to_dict(orient="records"))
+    # parâmetros de paginação
+    page = int(request.args.get("page", 1))
+    limit = int(request.args.get("limit", 10))
+
+    start = (page - 1) * limit
+    end = start + limit
+
+    dados_paginados = df.iloc[start:end]
+
+    return jsonify({
+        "data": dados_paginados.to_dict(orient="records"),
+        "page": page,
+        "limit": limit,
+        "total": len(df)
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)

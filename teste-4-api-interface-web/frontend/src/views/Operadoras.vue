f02da261ae20b0420a8cdd955b2b024be520b687
João Pedro Andrade <jopedro172@gmail.com>
2026-02-03 19:21:11 -0300

    exibe operadoras em tabela e ajusta consumo da API

--- teste-4-api-interface-web/frontend/src/views/Operadoras.vue
+++ teste-4-api-interface-web/frontend/src/views/Operadoras.vue
@@ -1,13 +1,47 @@
 <template>
   <div class="operadoras">
     <h2>Lista de Operadoras</h2>
-    <p>Esta será a tela principal da aplicação.</p>
+
+    <table v-if="operadoras">
+      <thead>
+        <tr>
+          <th>CNPJ</th>
+          <th>Razão Social</th>
+          <th>UF</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="op in operadoras.data" :key="op.CNPJ">
+          <td>{{ op.CNPJ }}</td>
+          <td>{{ op.RAZAO_SOCIAL }}</td>
+          <td>{{ op.UF }}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <p v-else>Carregando dados...</p>
   </div>
 </template>
 
 <script>
+import { getOperadoras } from '../services/api.js'
+
 export default {
-  name: 'Operadoras'
+  name: 'Operadoras',
+
+  data() {
+    return {
+      operadoras: null
+    }
+  },
+
+  async mounted() {
+    try {
+      this.operadoras = await getOperadoras()
+    } catch (error) {
+      console.error(error)
+    }
+  }
 }
 </script>
 
